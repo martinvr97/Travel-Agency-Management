44101872b130dfc7be7f43db1958debfc772ea77
martinvr97 <martinvreshtazi@gmail.com>
2025-11-02 10:48:39 +0100

    Initial commit - setup project structure

---
+++ src/test/java/com/Travel/Agency/Management/Travel/Agency/Management/TravelAgencyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Travel.Agency.Management.Travel.Agency.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
