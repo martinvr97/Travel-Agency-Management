2ac5f6115d4467c6e1cd0dd66d21573391b6f5be
martinvr97 <martinvreshtazi@gmail.com>
2025-11-08 14:32:07 +0100

    Repositories creation

---
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/repository/TravelPackageRepository.java
@@ -0,0 +1,13 @@
+package com.Travel.Agency.Management.Travel.Agency.Management.repository;
+
+import com.Travel.Agency.Management.Travel.Agency.Management.model.entity.TravelPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TravelPackageRepository extends JpaRepository<TravelPackage, Long> {
+    List<TravelPackage> findByDestinationContainingIgnoreCase(String destination);
+}
+
