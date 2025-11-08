4b4e8e1a88037e28ad22f987ebcf813bc990cddf
martinvr97 <martinvreshtazi@gmail.com>
2025-11-08 11:26:58 +0100

    population of entity User and creation of entity Payment

--- src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/User.java
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/User.java
@@ -1,4 +1,19 @@
 package com.Travel.Agency.Management.Travel.Agency.Management.model.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDate birthday;
+    private String nationality;
+    private Gender gender;
+    private Role role;
+    private LocalDateTime createdAt;
+    private Set<Booking> bookings;
+    private Set<Review> reviews;
 }
