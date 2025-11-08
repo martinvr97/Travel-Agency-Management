5fd9a3c2ed4de3d327a2a017b52a7a1422d59c4a
albaselimaj15-dot <albaselimaj15@gmail.com>
2025-11-08 11:31:14 +0100

    Creation of entity travel package and add pom dependences

---
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/TravelPackage.java
@@ -0,0 +1,38 @@
+package com.Travel.Agency.Management.Travel.Agency.Management.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TravelPackage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String destination;
+    private Category category;
+    private String description;
+    private LocalDate startdate;
+    private LocalDate endDate;
+    private int capacity;
+    private int availableSlots;
+    private double priceperPerson;
+    private double averageRating;
+    private int reviewCount;
+    private Set<Facility> facilities;
+    private List<String> images;
+    private LocalDateTime createdAt;
+    private List<Booking> bookings;
+    private List<Review> reviews;
+
+}
