fdb935138e7918efbb4188a3a6136e999b132b45
martinvr97 <martinvreshtazi@gmail.com>
2025-11-08 12:28:12 +0100

    relationship modification

--- src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/TravelPackage.java
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/TravelPackage.java
@@ -1,39 +1,54 @@
 package com.Travel.Agency.Management.Travel.Agency.Management.model.entity;
 
 import com.Travel.Agency.Management.Travel.Agency.Management.model.enums.Facility;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "travel_packages")
 public class TravelPackage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String destination;
     private String description;
-    private LocalDate startdate;
+    private LocalDate startDate;
     private LocalDate endDate;
     private int capacity;
     private int availableSlots;
-    private double priceperPerson;
+    private double pricePerPerson;
     private double averageRating;
     private int reviewCount;
-    private Set<Facility> facilities;
-    private List<String> images;
-    private LocalDateTime createdAt;
-    private List<Booking> bookings;
-    private List<Review> reviews;
 
+    //Create a separate table that stores all the facilities for each travel package.
+    @ElementCollection(targetClass = Facility.class)
+    @Enumerated(EnumType.STRING)
+    private Set<Facility> facilities;
+
+    //Create a separate table that stores all the images for each travel package.
+    @ElementCollection
+    private List<String> images;
+
+    private LocalDateTime createdAt;
+
+
+    @OneToMany(mappedBy = "travelPackage", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
+
+    @OneToMany(mappedBy = "travelPackage", cascade = CascadeType.ALL)
+    private List<Review> reviews;
 }
--- src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/User.java
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/model/entity/User.java
@@ -2,13 +2,25 @@ package com.Travel.Agency.Management.Travel.Agency.Management.model.entity;
 
 import com.Travel.Agency.Management.Travel.Agency.Management.model.enums.Gender;
 import com.Travel.Agency.Management.Travel.Agency.Management.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -16,9 +28,18 @@ public class User {
     private String password;
     private LocalDate birthday;
     private String nationality;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
+
     private LocalDateTime createdAt;
-    private Set<Booking> bookings;
-    private Set<Review> reviews;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Booking> bookings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
 }
