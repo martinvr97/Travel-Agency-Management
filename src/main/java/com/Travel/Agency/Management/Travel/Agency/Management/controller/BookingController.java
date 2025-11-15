c2afd1f3b3e4cd0a6df7a3dc5b48844334d12c81
martinvr97 <martinvreshtazi@gmail.com>
2025-11-15 14:49:55 +0100

    booking list page

---
+++ src/main/java/com/Travel/Agency/Management/Travel/Agency/Management/controller/BookingController.java
@@ -0,0 +1,40 @@
+package com.Travel.Agency.Management.Travel.Agency.Management.controller;
+
+import com.Travel.Agency.Management.Travel.Agency.Management.model.entity.Booking;
+import com.Travel.Agency.Management.Travel.Agency.Management.model.entity.User;
+import com.Travel.Agency.Management.Travel.Agency.Management.repository.BookingRepository;
+import com.Travel.Agency.Management.Travel.Agency.Management.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
+
+    @GetMapping("/my")
+    public String viewMyBookings(Authentication authentication, Model model) {
+        //Get current user
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        //List all bookings
+        List<Booking> userBookings = bookingRepository.findByUser(user);
+
+        //Add data to the model
+        model.addAttribute("bookings", userBookings);
+        model.addAttribute("user", user);
+        return "my-bookings";
+    }
+}
